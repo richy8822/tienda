e925b7ebf37c48588a735887bf01c24a12bdd0b5
Ricard <developer2@swingmaniacs.com>
2017-02-13 14:30:44 +0100

    Iniciemos

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+  <body>
+    <div class="fondo">
+      <?php echo "hola"; ?>
+
+    </div>
+
+  </body>
+</html>
